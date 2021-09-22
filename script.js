8400e1add0c270a0ae4b78fed6609d5efc3282c1
Gabriel Lantin <luigilantin@gmail.com>
2021-09-22 01:21:28 -0700

    pushing my comments

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var characters = prompt("How many characters would you like your password to be?");
+  checkCharacters(characters);
+  console.log(characters)
+  // check special characters (uppercase, lowercase, numeric, special characters)
+  // 
+}
+
+function checkCharacters(numCharacters) {
+  if (numCharacters != null && Number.isInteger(numCharacters) && numCharacters <= 128 && numCharacters >= 8) {
+    console.log("good", numCharacters)
+      return numCharacters;
+  } else {
+    let number = prompt("Please enter a number");
+    console.log("error", number)
+    checkCharacters(number);
+  }
+    // if (numCharacters <= 128 && numCharacters >= 8) {
+    //   console.log("good", numCharacters)
+    //   return numCharacters;
+    // } else {
+    //   let number = prompt("Please enter a number");
+    //   console.log("error", number)
+    //   checkCharacters(number);
+    // }
+  // } else {
+  //   let number = prompt("Please enter a number");
+  //   console.log("error", number)
+  //   checkCharacters(number);
+  // }
+  // return;
+}
+
+// function 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
